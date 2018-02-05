1f08fb5a1824656084a451166be0411b0f88c06e
Boas.Xiao <yxhlfx@gmail.com>
2018-02-05 21:35:47 +0800

    update for some details

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # CONTENT
 ## Python
-1. A tool for flushing the view count of csdn blog.
 2. A tool that auto to install the sublime3 in Linux.
 ## Shell
 1. A tool that auto install and configration shawdowsocks.
--- python/1/flush_view.py
+++ python/1/flush_view.py
@@ -20,7 +20,7 @@ Cache-Control: max-age=0
 Upgrade-Insecure-Requests: 1
 User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
-Referer: http://blog.csdn.net/yxhlfx
+Referer: http://blog.csdn.net/yourname
 Accept-Encoding: gzip, deflate
 Accept-Language: en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2
 '''
